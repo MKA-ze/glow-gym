470b8d91c65318f3cb558d461e9bb071852c5950
MKA-ze <kahmeronzemichael@gmail.com>
2025-06-24 23:57:02 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
     </main>
-    <div class="site-footer">
+    <div class="footer">
         <p><b>All rights reserved &copy;glow_gym2025</b></p>
         <p><b>"Push yourself, because no one else is going to do it for you."</b></p>
         <p><b>Contact us: glowgymcmc.com</b></p>
